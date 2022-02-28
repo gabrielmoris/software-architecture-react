eb7874cd7bab32ae399d5a365ae9ebf3c9b42ce0
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-02-28 20:42:36 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import {Home} from "./src/pages/Home"
+
+const app = express();
+
+app.use(express.static("./build",{index:false}))
+
+app.get("/*", (req, res) => {
+    const reactApp = renderToString(<Home></Home>);
+
+    return res.send(`
+    <html>
+        <body>
+            <div id="root">${reactApp}</div>      
+        </body>
+    </html>
+    `);
+});
+
+app.listen(8080,()=>{
+    console.log("server is listenning on port 8080")
+})
