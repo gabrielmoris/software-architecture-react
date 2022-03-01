8f4f4bcc6ad46fbccd6c4a0231538c99aa3b90e9
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-03-01 09:00:50 +0100

    add styles, and server

--- server.js
+++ server.js
@@ -1,24 +1,44 @@
 import express from "express";
 import React from "react";
+import { ServerStyleSheet } from "styled-components";
 import { renderToString } from "react-dom/server";
-import {Home} from "./src/pages/Home"
+import { StaticRouter } from "react-router-dom";
+import path from "path";
+import fs from "fs";
+import App from "./src/App";
 
 const app = express();
 
-app.use(express.static("./build",{index:false}))
+app.use(express.static("./build", { index: false }));
 
-app.get("/*", (req, res) => {
-    const reactApp = renderToString(<Home></Home>);
+app.get('/*', (req, res) => {
+    const sheet = new ServerStyleSheet();
 
-    return res.send(`
-    <html>
-        <body>
-            <div id="root">${reactApp}</div>      
-        </body>
-    </html>
-    `);
+    const reactApp = renderToString(
+        sheet.collectStyles(
+            <StaticRouter location={req.url}>
+                <App/>
+            </StaticRouter>
+        )
+    );
+
+    const templateFile = path.resolve("./build/index.html");
+    fs.readFile(templateFile, "utf8", (err, data) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        return res.send(
+            data
+                .replace(
+                    `<div id="root></div>`,
+                    `<div id="root">${reactApp}</div>`
+                )
+                .replace("{{ styles }}", sheet.getStyleTags())
+        );
+    });
 });
 
-app.listen(8080,()=>{
-    console.log("server is listenning on port 8080")
-})
+app.listen(8080, () => {
+    console.log("server is listenning on port 8080");
+});
--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { Articles } from './pages/Articles';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Articles } from "./pages/Articles";
+import styled from "styled-components";
+
+const BigRedHeading = styled.h1`
+    color: red;
+    font-size: 50px;
+`;
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<h1>Server-Side Rendering Example</h1>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/about">About</Link>
-				</li>
-				<li>
-					<Link to="/articles">Articles</Link>
-				</li>
-			</ul>
-			<Switch>
-				<Route path="/" exact>
-					<Home />
-				</Route>
-				<Route path="/about">
-					<About />
-				</Route>
-				<Route path="/articles">
-					<Articles />
-				</Route>
-			</Switch>
-		</BrowserRouter>
-	);
-}
+    return (
+        <>
+            <BigRedHeading>Server-Side Rendering Example</BigRedHeading>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/articles">Articles</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/articles">
+                    <Articles />
+                </Route>
+            </Switch>
+        </>
+    );
+};
 
 export default App;
+
+// npx nodemon --exec npx babel-node server.js
